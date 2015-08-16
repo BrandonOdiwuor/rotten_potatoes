b9f28dbe5e7922316ff28a81a7284f996125a4b2
saasbook <saasbook@saasbook.(none)>
2015-08-16 12:59:10 +0000

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,18 +7,56 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = MoviesController.ratings    
+    
+ #    params.each { |p|
+ #       session[p] = p
+ #    }
+#      redirect_to movies_path(params => session)
+#    end
+    
     @movies = Movie.all
-    if params[:sort_by_title] then      
-      @movies.sort! {|a,b| a.title <=> b.title}
-    elsif params[:sort_by_date] then      
-      @movies.sort! {|a,b| a.release_date <=> b.release_date}
-    elsif params[:ratings] then
-      @movies = MoviesController.filter_by_ratings(params[:ratings])
-    else
-      @movies = Movie.all
+    @redirect = 0
+    if(@checked != nil)
+      @movies = @movies.find_all{ |m| @checked.has_key?(m.rating) and  @checked[m.rating]==true}      
     end
+    
+    
+   if(params[:sort].to_s == 'title')
+    session[:sort] = params[:sort]
+    @movies = @movies.sort_by{|m| m.title }
+   elsif(params[:sort].to_s == 'release')
+    session[:sort] = params[:sort]
+    @movies = @movies.sort_by{|m| m.release_date.to_s }
+   elsif(session.has_key?(:sort) )
+    params[:sort] = session[:sort]
+    @redirect = 1
+   end
+    
+    
+    if(params[:ratings] != nil)
+      session[:ratings] = params[:ratings]
+      @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+    elsif(session.has_key?(:ratings) )
+      params[:ratings] = session[:ratings]
+      @redirect =1
+    end
+    
+    if(@redirect ==1)
+    redirect_to movies_path(:sort=>params[:sort], :ratings =>params[:ratings] )
+    end
+
+    @checked = {}
+    @all_ratings =  ['G','PG','PG-13','R']
+
+    @all_ratings.each { |rating|
+      if params[:ratings] == nil
+        @checked[rating] = false
+      else
+        @checked[rating] = params[:ratings].has_key?(rating)
+      end
+    }
   end
+
 
   def self.filter_by_ratings(rate)    
     result = Array.new 
